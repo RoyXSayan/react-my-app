9e66c330dd2aa8754d32f9c49bf8db05382763a5
Sayan Roy <105128169+UnusualSayan@users.noreply.github.com>
2024-07-19 16:19:57 +0530

    Updates

---
+++ precache-manifest.d0ead2f7317624fd5e2585d313065ea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c35e3e8b6b652eae0b84d6b5c780dbc",
+    "url": "/react-my-app/index.html"
+  },
+  {
+    "revision": "8cfe4c47ff3048b6d1bc",
+    "url": "/react-my-app/static/css/main.aee43274.chunk.css"
+  },
+  {
+    "revision": "69c5e342a0b0fc00ba6b",
+    "url": "/react-my-app/static/js/2.9eb6f66f.chunk.js"
+  },
+  {
+    "revision": "b08455c71dc97f4a4f6ed67eafe0e6f7",
+    "url": "/react-my-app/static/js/2.9eb6f66f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d0bbc9c39c619dd2d5",
+    "url": "/react-my-app/static/js/3.1da997f3.chunk.js"
+  },
+  {
+    "revision": "8cfe4c47ff3048b6d1bc",
+    "url": "/react-my-app/static/js/main.e6d2796c.chunk.js"
+  },
+  {
+    "revision": "fdc2d138f77999bb50a9",
+    "url": "/react-my-app/static/js/runtime-main.2946618e.js"
+  }
+]);
